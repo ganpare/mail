e5529f8c51db00a781be314b199e3a994e28d9ff
nobu007 <contact645@gmail.com>
2024-12-12 22:28:14 +0900

    かなり近づいてきた復活に

--- smtp/init.sh
+++ smtp/init.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 echo "Test script is running!"
 
+# rsyslog の起動 (フォアグラウンドでなくデーモン化する場合は単に rsyslogd でOK)
+rsyslogd
+
 # ファイル確認
 ls -l /var/mail
 ls -l /etc/postfix
@@ -12,6 +15,6 @@ postfix check
 echo "Starting Postfix..."
 postfix start
 
-# Dovecotの起動
+# Dovecotの起動（-Fオプションでフォアグラウンド実行）
 echo "Starting Dovecot..."
 exec dovecot -F
